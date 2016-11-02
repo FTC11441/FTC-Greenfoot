140259f32ed373e0fa9a9ab8adc1510880215bcd
Ethan Hampton <ethan.hampton@msd.oregonk-12.net>
2016-11-02 16:19:24 -0700

    Started adding simple Robot actor

---
+++ FTC Greenfoot/src/com/ethohampton/FTCGreenfoot/Actor/Motor.java
@@ -0,0 +1,17 @@
+package com.ethohampton.FTCGreenfoot.Actor;
+
+public class Motor {
+	
+	int power;
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+	
+	
+
+}
---
+++ FTC Greenfoot/src/com/ethohampton/FTCGreenfoot/Actor/Robot.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.ethohampton.FTCGreenfoot.Actor;
+
+import greenfoot.Actor;
+
+/**
+ * @author ethan.hampton
+ *
+ */
+public class Robot extends Actor {
+		public Motor leftMotor = new Motor();
+		public Motor rightMotor = new Motor();
+		
+		
+
+}
