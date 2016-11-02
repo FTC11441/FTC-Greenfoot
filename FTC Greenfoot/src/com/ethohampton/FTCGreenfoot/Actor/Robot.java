0e472eb9215df23fd8876c03d29c3baff752bdf1
Ethan Hampton <ethan.hampton@msd.oregonk-12.net>
2016-11-02 16:39:22 -0700

    added date

--- FTC Greenfoot/src/com/ethohampton/FTCGreenfoot/Actor/Robot.java
+++ FTC Greenfoot/src/com/ethohampton/FTCGreenfoot/Actor/Robot.java
@@ -6,13 +6,10 @@ package com.ethohampton.FTCGreenfoot.Actor;
 import greenfoot.Actor;
 
 /**
- * @author ethan.hampton
- *
+ * @author ethan.hampton 11-2-16
  */
 public class Robot extends Actor {
-		public Motor leftMotor = new Motor();
-		public Motor rightMotor = new Motor();
-		
-		
+	public Motor leftMotor = new Motor();
+	public Motor rightMotor = new Motor();
 
 }
